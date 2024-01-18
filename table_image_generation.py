3bfa0d8164b7f40c1971b4ff3e2342e1e9d999f3
JohnMcCodeman5 <milanbrcin5@gmail.com>
2024-01-18 02:24:18 +0100

    tested all methods on all datasets and created comp tables, minor fixes

--- table_image_generation.py
+++ table_image_generation.py
@@ -25,19 +25,22 @@ table.auto_set_font_size(False)
 table.set_fontsize(9)
 plt.savefig('table_images/best_table2.png', bbox_inches='tight', pad_inches=0.1)
 
-
+#-------------------------------------------------
 dfa = pd.read_csv('comparison_tables/averages.csv')
 dfa1 = dfa[columns]
 dfa2 = dfa.drop(columns[1:], axis=1)
 
+fig1, ax1 = plt.subplots(figsize=(10, 4)) 
+# Hide axes
+ax1.axis('off')
 
-table = pd.plotting.table(ax, dfa1, loc='center', colWidths=[0.2]*len(dfa1.columns))
-table.auto_set_font_size(False)
-table.set_fontsize(9)
+table1 = pd.plotting.table(ax1, dfa1, loc='center', colWidths=[0.2]*len(dfa1.columns))
+table1.auto_set_font_size(False)
+table1.set_fontsize(9)
 plt.savefig('table_images/avg_table1.png', bbox_inches='tight', pad_inches=0.1)
 
 
-table = pd.plotting.table(ax, dfa2, loc='center', colWidths=[0.2]*len(dfa2.columns))
-table.auto_set_font_size(False)
-table.set_fontsize(9)
+table1 = pd.plotting.table(ax1, dfa2, loc='center', colWidths=[0.2]*len(dfa2.columns))
+table1.auto_set_font_size(False)
+table1.set_fontsize(9)
 plt.savefig('table_images/avg_table2.png', bbox_inches='tight', pad_inches=0.1)
